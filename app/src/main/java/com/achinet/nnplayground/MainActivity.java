bba82e9bbfe720f627e72aa1354e407fd2e945d4
Achilleas <achilleas.k@gmail.com>
2015-10-16 04:27:06 +0300

    Organise imports

--- app/src/main/java/com/achinet/nnplayground/MainActivity.java
+++ app/src/main/java/com/achinet/nnplayground/MainActivity.java
@@ -3,18 +3,13 @@ package com.achinet.nnplayground;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
